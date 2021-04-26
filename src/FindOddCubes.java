dc5281e2be97e2a01ab4988aee776465f4db7efb
= <=>
2021-04-27 00:53:58 +0100

    second day-1 algo

--- src/FindOddCubes.java
+++ src/FindOddCubes.java
@@ -1,5 +1,10 @@
 import java.util.Arrays;
+
 // Day-1
+// Find the sum of the odd numbers within an array, after cubing the initial integers.
+// The function should return undefined/None/nil/NULL if any of the values aren't numbers.
+// Note: There are ONLY integers in the JAVA and C# versions of this Kata.
+
 public class FindOddCubes {
     public static void main(String[] args) {
         System.out.println(FindOddCubes.cubeOdd(new int[] {-5, -5, 5, 5}));
