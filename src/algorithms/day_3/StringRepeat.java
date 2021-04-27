99dece334e868c600e336be6ad0af67b11c136ae
= <=>
2021-04-27 16:31:09 +0100

    Solved String repeat algorithm challenge

---
+++ src/algorithms/day_3/StringRepeat.java
@@ -0,0 +1,15 @@
+package algorithms.day_3;
+
+/*
+(String Repeat)
+Write a function called repeatStr which repeats the given string string exactly n times.
+
+repeatStr(6, "I") // "IIIIII"
+repeatStr(5, "Hello") // "HelloHelloHelloHelloHello"
+ */
+
+public class StringRepeat {
+    public static String repeatStr(final int repeat, final String string) {
+        return string.repeat(repeat);
+    }
+}
